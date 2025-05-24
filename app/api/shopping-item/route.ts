f7f0d22c9233a8bd9294fd41732d6577bd01023e
daxchx <daxchx.v1@gmail.com>
2025-05-24 11:19:21 +0900

    feat: apiルートを修正

--- app/api/shopping-item/route.ts
+++ app/api/shopping-item/route.ts
@@ -2,10 +2,11 @@ import { supabase } from '@/lib/supabaseClient';
 import { NextRequest } from 'next/server';
 
 export async function POST(req: Request) {
-  const { name, shopping_list_id } = await req.json();
+  const { name, shopping_list_id, category_id } = await req.json();
+  console.log(category_id);
   const { data } = await supabase
     .from('shopping_item')
-    .insert({ name: name, shopping_list_id: shopping_list_id })
+    .insert({ name: name, shopping_list_id: shopping_list_id, category_id: category_id })
     .select()
     .single();
 
@@ -19,4 +20,24 @@ export async function GET(req: NextRequest) {
   const { data } = await supabase.from('shopping_item').select('*').eq('shopping_list_id', query);
 
   return Response.json(data);
+}
+
+export async function PUT(req: Request) {
+  const { id, name, checked, category_id } = await req.json();
+  const { data } = await supabase
+    .from('shopping_item')
+    .update({ name, checked, category_id })
+    .eq('id', id)
+    .select()
+    .single();
+
+  return Response.json(data);
+}
+
+export async function DELETE(req: Request) {
+  const { id } = await req.json();
+
+  await supabase.from('shopping_item').delete().eq('id', id);
+
+  return new Response(null, { status: 204 });
 }
