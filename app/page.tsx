a46ab64da299db80773184d090d727292fcc50cd
daxchx <daxchx.v1@gmail.com>
2025-06-30 10:24:04 +0900

    feat: トップ画面を作成 (#62)

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
-import { ShoppingBasket } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <>
-      <div className="p-4">
-        <h1 className="text-2xl font-bold">ホーム</h1>
+    <div className="mx-auto max-w-2xl">
+      <div className="flex justify-center">
+        <Image src="/top.png" alt="caumonoイメージ画像" width={400} height={400} />
       </div>
-      <div className="p-4">
-        <Link href="/shopping-list" className="flex rounded-md border border-neutral-200 p-4">
-          <ShoppingBasket className="text-sky-400" />
-          <span className="ml-2 font-bold text-sky-400">買い物リスト</span>
-        </Link>
+      <div>
+        <div className="text-center text-xl font-bold">”買うもの”をキレイに整理。</div>
+        <div className="px-6 py-2 text-sm">
+          買うものをカテゴリーや順番ごとに整理して、ムダなくスマートなお買い物をサポートします。
+        </div>
       </div>
-    </>
+      <div className="mt-4 flex items-center justify-center">
+        <Button asChild>
+          <Link href="/shopping-list">はじめる</Link>
+        </Button>
+      </div>
+    </div>
   );
 }
